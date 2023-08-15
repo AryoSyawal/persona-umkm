b7c58f79ca4ac10262eb28d3f2a9f4cf65a92ff0
Angga Ferdani <eanggawahyu@gmail.com>
2023-08-15 13:46:48 +0700

    integrate dashboard template, login and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware(['web', 'disableBackButton'])->group(function(){
+    Route::middleware(['authenticated'])->group(function(){
+        Route::get('/login', [AuthenticationController::class, 'login'])->name('login');
+        Route::post('/post-login', [AuthenticationController::class, 'postLogin'])->name('post-login');
+    });
+    
+    Route::get('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+});
+
+Route::prefix('superadmin')->name('superadmin.')->group(function(){
+    Route::middleware(['auth:web', 'disableBackButton', 'superadmin'])->group(function(){
+        Route::get('/dashboard', function(){ return view('pages.dashboard'); })->name('dashboard');
+    });
+});
+
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::middleware(['auth:web', 'disableBackButton', 'admin'])->group(function(){
+        Route::get('/dashboard', function(){ return view('pages.dashboard'); })->name('dashboard');
+    });
+});
